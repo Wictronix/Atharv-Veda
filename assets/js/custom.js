552aee4af4b59621681493ebdb6a9cccb01da032
ViveK Poonia <vivekpoonia2112@gmail.com>
2025-10-30 16:03:52 +0530

    Add floating contact buttons and update logo
    Introduces floating WhatsApp and Call Now contact buttons with supporting CSS and JS. Updates logo references to 'New-Logo.png' in HTML and removes the old logo image. Also adds Tawk.to live chat integration and refines JS code formatting and event handling.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,7 +29,7 @@ Project: Atharv Veda HTML
 
     // loader js
     ayur_loader: function () {
-      jQuery(window).on('load', function () {
+      jQuery(window).on("load", function () {
         $(".ayur-loader").fadeOut();
         $(".ayur-spin").delay(500).fadeOut("slow");
       });
@@ -47,24 +47,24 @@ Project: Atharv Veda HTML
       });
 
       //submenu
-      $('.ayur-has-menu > a').click(function (event) {
+      $(".ayur-has-menu > a").click(function (event) {
         event.stopPropagation();
-        var $submenu = $(this).next('.ayur-submenu');
-        $('.ayur-submenu').not($submenu).removeClass('ayur-submenu-open');
-        $submenu.toggleClass('ayur-submenu-open');
+        var $submenu = $(this).next(".ayur-submenu");
+        $(".ayur-submenu").not($submenu).removeClass("ayur-submenu-open");
+        $submenu.toggleClass("ayur-submenu-open");
       });
-      $('.ayur-submenu a').click(function (event) {
+      $(".ayur-submenu a").click(function (event) {
         event.stopPropagation();
       });
       $(document).on("click", function (event) {
-        if (!$(event.target).closest('.ayur-has-menu').length) {
-          $('.ayur-submenu').removeClass('ayur-submenu-open')
+        if (!$(event.target).closest(".ayur-has-menu").length) {
+          $(".ayur-submenu").removeClass("ayur-submenu-open");
         }
-      })
+      });
     },
     // fix menu scroll
     tr_fix_menu_scroll: function () {
-      $(window).on('scroll', function () {
+      $(window).on("scroll", function () {
         if ($(window).scrollTop() > 300) {
           $(".tr_menu_wrapper1").addClass("tr-fixed");
         } else {
@@ -126,12 +126,11 @@ Project: Atharv Veda HTML
           },
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
       });
     },
-
 
     // care slider
     ayur_care_slider: function () {
@@ -140,8 +139,8 @@ Project: Atharv Veda HTML
         slidesPerView: 8,
         spaceBetween: 30,
         navigation: {
-          nextEl: '.ayur-care-slider-sec .swiper-button-next',
-          prevEl: '.ayur-care-slider-sec .swiper-button-prev',
+          nextEl: ".ayur-care-slider-sec .swiper-button-next",
+          prevEl: ".ayur-care-slider-sec .swiper-button-prev",
         },
         breakpoints: {
           1800: {
@@ -195,8 +194,8 @@ Project: Atharv Veda HTML
           delay: 2000,
         },
         navigation: {
-          nextEl: '.ayur-testimonial-section .swiper-button-next',
-          prevEl: '.ayur-testimonial-section .swiper-button-prev',
+          nextEl: ".ayur-testimonial-section .swiper-button-next",
+          prevEl: ".ayur-testimonial-section .swiper-button-prev",
         },
         breakpoints: {
           768: {
@@ -212,15 +211,15 @@ Project: Atharv Veda HTML
     },
 
     ayur_counter: function () {
-      $('.ayur-counting').text('');
-      $('.ayur-counting').each(function () {
+      $(".ayur-counting").text("");
+      $(".ayur-counting").each(function () {
         var $this = $(this),
           countTo = $this.attr("data-to");
         $({ countNum: $this.text() }).animate(
-          { countNum: countTo, },
+          { countNum: countTo },
           {
             duration: 3000,
-            easing: 'linear',
+            easing: "linear",
             step: function () {
               $this.text(Math.floor(this.countNum));
             },
@@ -228,84 +227,84 @@ Project: Atharv Veda HTML
               $this.text(this.countNum);
             },
           }
-        )
-      })
-
+        );
+      });
     },
     ayur_quantity: function () {
-      $('.shop-add').click(function () {
+      $(".shop-add").click(function () {
         if ($(this).prev().val() < 50000) {
-          $(this).prev().val(+$(this).prev().val() + 1);
+          $(this)
+            .prev()
+            .val(+$(this).prev().val() + 1);
         }
       });
-      $('.shop-sub').click(function () {
-        let inputElement = $(this).prevAll('input');
+      $(".shop-sub").click(function () {
+        let inputElement = $(this).prevAll("input");
         if (inputElement.val() > 1) {
           inputElement.val(+inputElement.val() - 1);
         }
       });
-
     },
     Select2: function () {
-      $('.mySelect').select2({
-        placeholder: 'Select an option',
-        width: '100%',
+      $(".mySelect").select2({
+        placeholder: "Select an option",
+        width: "100%",
         dropdownAutoWidth: true,
-        minimumResultsForSearch: Infinity
+        minimumResultsForSearch: Infinity,
       });
     },
-    // Checkout Payment			
+    // Checkout Payment
     CheckoutPayment: function () {
       $("input[name$='checkout']").on("click", function () {
         var test = $(this).val();
-        $(".payment_box").hide('slow');
-        $(".payment_box[data-period='" + test + "']").show('slow');
+        $(".payment_box").hide("slow");
+        $(".payment_box[data-period='" + test + "']").show("slow");
       });
     },
   };
   pureayurveda.init();
-
-}
-  (jQuery));
-
+})(jQuery);
 
 function checkRequire(formId, targetResp) {
-  targetResp.html('');
-  var email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-  var url = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+  targetResp.html("");
+  var email =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+  var url =
+    /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
   var image = /\.(jpe?g|gif|png|PNG|JPE?G)$/;
   var mobile = /^[\s()+-]*([0-9][\s()+-]*){6,20}$/;
   var facebook = /^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]/;
   var twitter = /^(https?:\/\/)?(www\.)?twitter.com\/[a-zA-Z0-9(\.\?)?]/;
-  var google_plus = /^(https?:\/\/)?(www\.)?plus.google.com\/[a-zA-Z0-9(\.\?)?]/;
+  var google_plus =
+    /^(https?:\/\/)?(www\.)?plus.google.com\/[a-zA-Z0-9(\.\?)?]/;
   var check = 0;
-  $('#er_msg').remove();
-  var target = (typeof formId == 'object') ? $(formId) : $('#' + formId);
-  target.find('input , textarea , select').each(function () {
-    if ($(this).hasClass('require')) {
-      if ($(this).val().trim() == '') {
+  $("#er_msg").remove();
+  var target = typeof formId == "object" ? $(formId) : $("#" + formId);
+  target.find("input , textarea , select").each(function () {
+    if ($(this).hasClass("require")) {
+      if ($(this).val().trim() == "") {
         check = 1;
         $(this).focus();
-        $(this).parent('div').addClass('form_error');
-        targetResp.html('You missed out some fields.');
-        $(this).addClass('error');
+        $(this).parent("div").addClass("form_error");
+        targetResp.html("You missed out some fields.");
+        $(this).addClass("error");
         return false;
       } else {
-        $(this).removeClass('error');
-        $(this).parent('div').removeClass('form_error');
+        $(this).removeClass("error");
+        $(this).parent("div").removeClass("form_error");
       }
     }
-    if ($(this).val().trim() != '') {
-      var valid = $(this).attr('data-valid');
-      if (typeof valid != 'undefined') {
+    if ($(this).val().trim() != "") {
+      var valid = $(this).attr("data-valid");
+      if (typeof valid != "undefined") {
         if (!eval(valid).test($(this).val().trim())) {
-          $(this).addClass('error');
+          $(this).addClass("error");
           $(this).focus();
           check = 1;
-          targetResp.html($(this).attr('data-error'));
+          targetResp.html($(this).attr("data-error"));
           return false;
         } else {
-          $(this).removeClass('error');
+          $(this).removeClass("error");
         }
       }
     }
@@ -313,37 +312,103 @@ function checkRequire(formId, targetResp) {
   return check;
 }
 
-$(".submitForm").on('click', function () {
+$(".submitForm").on("click", function () {
   var _this = $(this);
-  var targetForm = _this.closest('form');
-  var errroTarget = targetForm.find('.response');
+  var targetForm = _this.closest("form");
+  var errroTarget = targetForm.find(".response");
   var check = checkRequire(targetForm, errroTarget);
 
   if (check == 0) {
     var formDetail = new FormData(targetForm[0]);
-    formDetail.append('form_type', _this.attr('form-type'));
+    formDetail.append("form_type", _this.attr("form-type"));
     $.ajax({
-      method: 'post',
-      url: 'ajaxmail.php',
+      method: "POST",
+      url: "ajaxmail.php",
       data: formDetail,
       cache: false,
       contentType: false,
-      processData: false
+      processData: false,
     }).done(function (resp) {
       console.log(resp);
       if (resp == 1) {
-        targetForm.find('input').val('');
-        targetForm.find('textarea').val('');
-        errroTarget.html('<p style="color:green;">Mail has been sent successfully.</p>');
+        targetForm.find("input").val("");
+        targetForm.find("textarea").val("");
+        errroTarget.html(
+          '<p style="color:green;">Mail has been sent successfully.</p>'
+        );
       } else {
-        errroTarget.html('<p style="color:red;">Something went wrong please try again latter.</p>');
+        errroTarget.html(
+          '<p style="color:red;">Something went wrong please try again latter.</p>'
+        );
       }
     });
   }
 });
 
 $(document).ready(function () {
-  $('.ayur-tpro-like a').click(function () {
-    $(this).toggleClass("likeproduct")
-  })
-});;
+  $(".ayur-tpro-like a").click(function () {
+    $(this).toggleClass("likeproduct");
+  });
+});
+
+/* Floating contact buttons injection (WhatsApp + Call Now)
+   Injected here so every page that loads assets/js/custom.js gets the buttons
+*/
+(function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    try {
+      var phone = "+12022021994";
+      var wa = "12022021994";
+
+      // Ensure a Font Awesome stylesheet that includes brand icons is present.
+      // If an existing font-awesome link is present we leave it; otherwise add a CDN link (FA 5.15.4 all.min.css).
+      try {
+        var hasFA = !!document.querySelector(
+          'link[href*="font-awesome"], link[href*="fontawesome"], link[href*="all.min.css"]'
+        );
+        if (!hasFA) {
+          var faLink = document.createElement("link");
+          faLink.rel = "stylesheet";
+          faLink.href =
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css";
+          // avoid an integrity attribute here so the resource won't be blocked if the hash changes
+          faLink.crossOrigin = "anonymous";
+          faLink.referrerPolicy = "no-referrer";
+          document.head.appendChild(faLink);
+        }
+      } catch (err) {
+        // non-fatal: continue and create buttons even if adding the stylesheet fails
+        console.warn("Could not ensure Font Awesome stylesheet:", err);
+      }
+
+      var container = document.createElement("div");
+      container.className = "ayur-float-contact";
+      // helper to replace a failed image with an icon HTML
+      window.__ayur_imgOnError =
+        window.__ayur_imgOnError ||
+        function (imgEl, html) {
+          try {
+            imgEl.outerHTML = html;
+          } catch (e) {
+            imgEl.style.display = "none";
+          }
+        };
+      // Use local images if present (assets/wp.png for WhatsApp, assets/call.png for Call)
+      // If the image fails to load, replace it with the corresponding FA icon as a fallback.
+      container.innerHTML =
+        '<a class="ayur-float-btn ayur-float-whatsapp" href="https://wa.me/' +
+        wa +
+        '?text=Hello%20Atharv%20Veda" target="_blank" rel="noopener noreferrer" aria-label="Chat on WhatsApp">' +
+        '<img src="assets/wp.png" alt="WhatsApp"/>' +
+        "<span>WhatsApp</span></a>" +
+        '<a class="ayur-float-btn ayur-float-call" href="tel:' +
+        phone +
+        '" aria-label="Call Now">' +
+        '<img src="assets/call.png" alt="Call"/>' +
+        "<span>Call Now</span></a>";
+      document.body.appendChild(container);
+    } catch (e) {
+      console.error("Error adding floating contact buttons:", e);
+    }
+  });
+})();
